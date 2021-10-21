126d6b50a8c64aa261c4d5aec9c71bd782647ec1
franco.balich@gmail.com <franco.balich@gmail.com>
2021-10-21 18:02:02 -0300

    Update code

--- Calculadora/Calculadora/Calculadora/MainPage.xaml.cs
+++ Calculadora/Calculadora/Calculadora/MainPage.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
 namespace Calculadora
 {
     public partial class MainPage : ContentPage
@@ -18,24 +17,30 @@ namespace Calculadora
             pkOper.Items.Add("Multiplicacion");
             pkOper.Items.Add("Division");
         }
-
         private void pkOper_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string op = pkOper.SelectedItem.ToString();
-            int n1 = Int32.Parse(txtN1.Text);
-            int n2 = Int32.Parse(txtN2.Text);
-            double res = 0;
-
-            if (op == "Suma")
-                res = n1 + n2;
-            else if (op == "Resta")
-                res = n1 - n2;
-            else if (op == "Multiplicacion")
-                res = n1 * n2;
-            else
-                res = n1 / n2;
-
-            lblRes.Text = res + "";
+            try
+            {
+                string op = pkOper.SelectedItem.ToString();
+                if (txtN1.Text!=""&& txtN2.Text!="")
+                {
+                    int n1 = Int32.Parse(txtN1.Text);
+                    int n2 = Int32.Parse(txtN2.Text);
+                    double res = 0;
+                    if (op == "Suma")
+                        res = n1 + n2;
+                    else if (op == "Resta")
+                        res = n1 - n2;
+                    else if (op == "Multiplicacion")
+                        res = n1 * n2;
+                    else
+                        res = n1 / n2;
+                    lblRes.Text = res + "";
+                }
+            }
+            catch (Exception)
+            { lblRes.Text=""; }
+            
         }
     }
 }
