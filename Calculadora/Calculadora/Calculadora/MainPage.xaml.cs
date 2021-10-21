4e340e75ca113c191dcb050e3d935621ae1d1675
franco.balich@gmail.com <franco.balich@gmail.com>
2021-10-21 17:26:19 -0300

    Add project

---
+++ Calculadora/Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            pkOper.Items.Add("Suma");
+            pkOper.Items.Add("Resta");
+            pkOper.Items.Add("Multiplicacion");
+            pkOper.Items.Add("Division");
+        }
+
+        private void pkOper_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string op = pkOper.SelectedItem.ToString();
+            int n1 = Int32.Parse(txtN1.Text);
+            int n2 = Int32.Parse(txtN2.Text);
+            double res = 0;
+
+            if (op == "Suma")
+                res = n1 + n2;
+            else if (op == "Resta")
+                res = n1 - n2;
+            else if (op == "Multiplicacion")
+                res = n1 * n2;
+            else
+                res = n1 / n2;
+
+            lblRes.Text = res + "";
+        }
+    }
+}
